283e6f9c99b52660003a89216daab2e0b5aee29f
LauritsFog <lause.fog@gmail.com>
2022-03-16 09:22:11 +0100

    Something

--- Problem_1.m
+++ Problem_1.m
@@ -1,8 +1,8 @@
 parm = [49,47,20.1,0.0106,0.0081,0.0022,1.33,253,47,5];
 
 tspan = linspace(1,24*60,24*60);
-D = zeros(1,length(T));
-U = ones(1,length(T)).*25.04;
+D = zeros(1,length(tspan));
+U = ones(1,length(tspan)).*25.04;
 
 x0 = zeros(1,7);
 
@@ -13,8 +13,11 @@ x0 = zeros(1,7);
 
 %%
 
-plot(T,X(6,:),'-');
+plot(tspan,X(6,:),'-');
 
 %%
 
-[T,X] = ode15s(@MVPmodel,tspan,x0,[],U,D,parm);
+[T,X] = ode15s(@MVPmodel,[0 24*60],x0',[U(1),D(1),parm]);
+
+%%
+
