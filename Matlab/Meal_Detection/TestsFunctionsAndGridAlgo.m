f9a2775e87e919b6f03d822992703a86b3f0c9a3
Frederik <s204213@win.dtu.dk>
2022-04-27 09:24:48 +0200

    iuyfihuv

--- Matlab/Meal_Detection/TestsFunctionsAndGridAlgo.m
+++ Matlab/Meal_Detection/TestsFunctionsAndGridAlgo.m
@@ -18,7 +18,15 @@ days=1;
 % Simulation of a human in "days" days
 % Need to change D1 in order to get other number of days
 [t,Gm]=Simulation(x0,Ts,days,D1,parm,1);
-%%
+
+figure(1)
+plot(t,Gm(:,4),"linewidth",3)
+xlabel("t [min]");
+ylabel("G [mg/dL]");
+title('Measured blood glucose consentration')
+set(gca)
+legend("Glucose concentration","Steady state","location","northeast")
+
 %plot(t,Gm(:,4))
 %dg=3;
 %dt=1;
@@ -26,6 +34,6 @@ dg=1;
 dt=1;
 %12 is arbitary chosen the parameter doesnt do anything in the function
 [GF,dGF,GRID]=GridAlgo(Gm(:,4),dg,dt,12,t);
-figure(3)
-plot(t,GRID);
+plot(t,GRID*350);
+
 
