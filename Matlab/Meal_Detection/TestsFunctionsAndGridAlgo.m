9fb7fb6ddab208d7c7359af5e18758924cbf8310
Frederik <s204213@win.dtu.dk>
2022-04-27 11:52:00 +0200

    Sidste push onsdag 27/04

--- Matlab/Meal_Detection/TestsFunctionsAndGridAlgo.m
+++ Matlab/Meal_Detection/TestsFunctionsAndGridAlgo.m
@@ -2,14 +2,11 @@
 loadLibrary()
 load('MealPlan.mat')
 %Create data for one day
-D1 = MealPlan(1);
+D1 = MealPlan(1,0);
 %% Default parameters
 x0=[1.24580,1.2458,0.010090,1.082110e+02,1.082110e+02,0,0];
 Ts=5;
 parm=[49;47;20.10;0.01060;0.00810;0.002200;1.3300;253;47;5];
-
-
-
 
 %% Testing GridAlgo
 
@@ -24,8 +21,7 @@ plot(t,Gm(:,4),"linewidth",3)
 xlabel("t [min]");
 ylabel("G [mg/dL]");
 title('Measured blood glucose consentration')
-set(gca)
-legend("Glucose concentration","Steady state","location","northeast")
+
 
 %plot(t,Gm(:,4))
 %dg=3;
@@ -33,6 +29,7 @@ legend("Glucose concentration","Steady state","location","northeast")
 dg=1;
 dt=1;
 %12 is arbitary chosen the parameter doesnt do anything in the function
+figure(3);
 [GF,dGF,GRID]=GridAlgo(Gm(:,4),dg,dt,12,t);
 plot(t,GRID*350);
 
@@ -47,10 +44,10 @@ subplot(2,2,1)
 hold on
 plot(t,Gm(:,4),"linewidth",3)
 plot(t,GRID*350);
-title("dg = 50 , dt = 50")
+title("dg = 1 , dt = 1")
 hold off
 
-dg=10;
+dg=3;
 dt=1;
 [GF,dGF,GRID]=GridAlgo(Gm(:,4),dg,dt,12,t);
 
@@ -58,11 +55,11 @@ subplot(2,2,2)
 hold on
 plot(t,Gm(:,4),"linewidth",3)
 plot(t,GRID*350);
-title("dg = 10 , dt = 1")
+title("dg = 3 , dt = 1")
 hold off
 
 dg=1;
-dt=10;
+dt=0.1;
 [t,Gm]=Simulation(x0,Ts,days,D1,parm,1);
 [GF,dGF,GRID]=GridAlgo(Gm(:,4),dg,dt,12,t);
 
@@ -70,14 +67,19 @@ subplot(2,2,3)
 hold on
 plot(t,Gm(:,4),"linewidth",3)
 plot(t,GRID*350);
-title("dg = 1 , dt = 1")
+title("dg = 1 , dt = 0.1")
 hold off
+
+dg=0.1;
+dt=0.1;
+[t,Gm]=Simulation(x0,Ts,days,D1,parm,1);
+[GF,dGF,GRID]=GridAlgo(Gm(:,4),dg,dt,12,t);
 
 subplot(2,2,4)
 hold on
 plot(t,Gm(:,4),"linewidth",3)
 plot(t,GRID*350);
-title("dg = 1 , dt = 30")
+title("dg = 0.1 , dt = 0.1")
 hold off
 
 %%Hej
